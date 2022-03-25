2d883f5702a992befb078bc0bb38c308e3e5b55c
Armen Gevorkyan <rediraph@mail.ru>
2022-03-25 23:54:54 +0700

    Init commit

---
+++ app/src/main/java/com/r3d1r4ph/visualnovel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.r3d1r4ph.visualnovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

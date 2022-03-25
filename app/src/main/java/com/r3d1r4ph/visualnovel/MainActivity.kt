e9eda4b1db187ee11916567954bb56e9062763c2
Armen Gevorkyan <rediraph@mail.ru>
2022-03-26 00:51:12 +0700

    configured system bars & added res colors

--- app/src/main/java/com/r3d1r4ph/visualnovel/MainActivity.kt
+++ app/src/main/java/com/r3d1r4ph/visualnovel/MainActivity.kt
@@ -2,10 +2,14 @@ package com.r3d1r4ph.visualnovel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.navigationBarColor = ContextCompat.getColor(this, android.R.color.transparent)
     }
 }

bb901b8d0acd8e59d4989b6d1291e81a560565df
Armen Gevorkyan <rediraph@mail.ru>
2022-03-27 00:25:58 +0700

    default fragment configured

--- app/src/main/java/com/r3d1r4ph/visualnovel/ui/fragments/DefaultFragment.kt
+++ app/src/main/java/com/r3d1r4ph/visualnovel/ui/fragments/DefaultFragment.kt
@@ -1,6 +1,8 @@
 package com.r3d1r4ph.visualnovel.ui.fragments
 
+import android.view.View
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.button.MaterialButton
 import com.r3d1r4ph.visualnovel.R
 import com.r3d1r4ph.visualnovel.databinding.FragmentDefaultBinding
 import com.r3d1r4ph.visualnovel.domain.Screen
@@ -9,8 +11,30 @@ class DefaultFragment : BaseFragment(R.layout.fragment_default) {
 
     override val viewBinding by viewBinding(FragmentDefaultBinding::bind)
 
-    override fun initViewByScreen(screen: Screen) {
+    override fun initViewByScreen(screen: Screen) = with(viewBinding) {
         super.initViewByScreen(screen)
+
+        defaultMessageTextView.text =
+            if (screen.id == 3) {
+                val name: String = arguments?.getString(NAME_ID) ?: getString(R.string.empty)
+                String.format(screen.message, name)
+            } else {
+                screen.message
+            }
+
+        actionGroup.referencedIds.forEachIndexed { index, id ->
+            val actionButton = root.findViewById<MaterialButton>(id)
+            if (screen.actions.size > index) {
+                with(actionButton) {
+                    visibility = View.VISIBLE
+
+                    text = screen.actions[index].message
+                    setOnClickListener {
+                        navigateByScreenId(screen.actions[index].toScreen)
+                    }
+                }
+            }
+        }
 
     }
 }
